0106068711a3835d98e89d71e5eb770c534d4b29
yeyu <1041255016@qq.com>
2015-12-22 23:18:43 +0800

    j

---
+++ api/Common/Common/BaseController.class.php
@@ -0,0 +1,11 @@
+<?php
+namespace common\common;
+use Think\Controller;
+class BaseController extends Controller {
+
+	public function __construct()
+	{
+		
+	}
+
+}
--- api/Common/Conf/config.php
+++ api/Common/Conf/config.php
@@ -1,4 +1,19 @@
 <?php
 return array(
 	//'配置项'=>'配置值'
+	'DEFAULT_GROUP'=>'Home',
+	 'DB_TYPE'               => 'mysql',     // 数据库类型
+    'DB_HOST'               => 'localhost', // 服务器地址
+    'DB_NAME'               => 'myproject',          // 数据库名
+    'DB_USER'               => 'root',      // 用户名  
+    'DB_PWD'                => 'root',          // 密码
+    'DB_PORT'               => '',        // 端口
+    'DB_PREFIX'             => 'tb_',    // 数据库表前缀
+	'DB_PREFIX'=>'tb_',
+
+	'TMPL_L_DELIM'=>'<{',
+	'TMPL_R_DELIM'=>'}>',
+	'URL_MODEL' => 1,
+	'SHOW_PAGE_TRACE' =>true,
 );
+?>
